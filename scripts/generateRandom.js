c1d494f1f15cb252e39431630dd889ea4f5d20ad
Your Name <kevin.kds80@gmail.com>
2020-11-22 16:23:29 -0300

    Created generate random color function to avoid an api usage and implemented the generate random pallete

--- scripts/generateRandom.js
+++ scripts/generateRandom.js
@@ -1,9 +1,11 @@
 const generate = async () => {
-    const response = await (await fetch("http://www.colr.org/json/scheme/random")).json();
-    const colors = response.schemes[0].colors;
     const ids = ["bckgnd", "main", "h1", "h2_h5", "links"];
-    ids.map((id, index) => {
-        console.log(id,colors[index])
-        saveProps({id, value: `#${colors[index]}`});
+    ids.map((id) => {
+        saveProps({id, value: getColor() });
     });
+}
+
+const getColor = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + randomColor;
 }
