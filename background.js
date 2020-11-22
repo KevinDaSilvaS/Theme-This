8bcced573e65d2193df461e6a7a38c7de6989a34
Your Name <kevin.kds80@gmail.com>
2020-11-22 20:18:12 -0300

    Removed unused code

--- background.js
+++ background.js
@@ -1,8 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -13,5 +9,3 @@ chrome.runtime.onInstalled.addListener(function() {
         }]);
     });
 });
-
-//http://www.colr.org/api.html
